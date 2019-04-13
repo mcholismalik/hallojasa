128c747f7acaba707423fd80259290cd8dc4d297
Cols <mcholismalik@gmail.com>
2019-04-13 15:11:23 +0700

    login - add simple & complex code

--- facade/authenticate.facade.js
+++ facade/authenticate.facade.js
@@ -14,44 +14,44 @@ const Customers = require('../model/customers.model')
 const router = express.Router()
 
 // complex but more informatif
-router.post('/login', validate(modelvalid.login), async (req, res) => {
-    try {
-        let data = await Customers.findOne({ where: { username: req.body.username } })
-        if (data) {
-            try {
-                let check = await bcrypt.compare(req.body.password, data.hash)
-                if (check == true) {
-                    try {
-                        let token = await Jwt.sign(config.payLoad, config.secretKey)
-                        res.json(ApiResponse.ok('Login success!', { token: token }))
-                    } catch (err) {
-                        res.json(ApiResponse.internalServerError('Jwt error', err))
-                    }
-                } else {
-                    res.json(ApiResponse.unAuthorized())
-                }
-            } catch (err) {
-                res.json(ApiResponse.internalServerError('Bcrypt error', err))
-            }
-        } else {
-            res.json(ApiResponse.notFound())
-        }
-    } catch (err) {
-        res.json(ApiResponse.internalServerError('Database Error', err))
-    }
-})
-
-// simple but less informatif
 // router.post('/login', validate(modelvalid.login), async (req, res) => {
 //     try {
 //         let data = await Customers.findOne({ where: { username: req.body.username } })
-//         let check = await bcrypt.compare(req.body.password, data.hash)
-//         let token = await Jwt.sign(config.payLoad, config.secretKey)
-//         res.json(ApiResponse.ok('Login success!', { token: token }))
+//         if (data) {
+//             try {
+//                 let check = await bcrypt.compare(req.body.password, data.hash)
+//                 if (check == true) {
+//                     try {
+//                         let token = await Jwt.sign(config.payLoad, config.secretKey)
+//                         res.json(ApiResponse.ok('Login success!', { token: token }))
+//                     } catch (err) {
+//                         res.json(ApiResponse.internalServerError('Jwt error', err))
+//                     }
+//                 } else {
+//                     res.json(ApiResponse.unAuthorized())
+//                 }
+//             } catch (err) {
+//                 res.json(ApiResponse.internalServerError('Bcrypt error', err))
+//             }
+//         } else {
+//             res.json(ApiResponse.notFound())
+//         }
 //     } catch (err) {
-//         res.json(ApiResponse.unknownError('Unknown Error', err))
+//         res.json(ApiResponse.internalServerError('Database Error', err))
 //     }
 // })
+
+// simple but less informatif
+router.post('/login', validate(modelvalid.login), async (req, res) => {
+    try {
+        let data = await Customers.findOne({ where: { username: req.body.username } })
+        let check = await bcrypt.compare(req.body.password, data.hash)
+        let token = await Jwt.sign(config.payLoad, config.secretKey)
+        res.json(ApiResponse.ok('Login success!', { token: token }))
+    } catch (err) {
+        res.json(ApiResponse.unknownError('Unknown Error', err))
+    }
+})
 
 router.post('/register', validate(modelvalid.register), async (req, res) => {
     try {
